4af956be76070b4c7679587dd234f73acdfc891b
Carlos López <carlos.armando.lopez.espinoza@uabc.edu.mx>
2026-02-01 00:53:45 -0800

    Correccion de  connectionString

--- GenericApp.API/Program.cs
+++ GenericApp.API/Program.cs
@@ -3,7 +3,7 @@ using GenericApp;
 var builder = WebApplication.CreateBuilder(args);
 
 var startup = new Startup(builder.Configuration);
-startup.ConfigurationServices(builder.Services);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
